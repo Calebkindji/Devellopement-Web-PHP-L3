00e253b8191c30a4f1bdb571a12e145d4f1afe0c
Calebkindji <23mk324si@esisalama.org>
2026-02-04 10:18:03 +0100

    Modify graphic interface

--- ind.php
+++ ind.php
@@ -3,9 +3,10 @@
 <h1 class="text-3xl font-bold text-center mt-10 text-white">Profiter des meilleurs offres</h1>
 <?php require 'models/promotionData.php'; ?>
 <?php foreach($promotions as $magasin => $produits): ?>
-    <div class="text-white">
+    <div class="text-white ">
         <h1><?php echo $magasin; ?></h1>
-        <?php foreach($produits as $produit => $details): ?>
+        <div class="grid grid-cols-3 gap-3">
+            <?php foreach($produits as $produit => $details): ?>
             <?php 
                 $prixInitial = $details['prix-Initial'];
                 $prixPromo   = $details['prix-promo'];
@@ -13,12 +14,11 @@
                 // Calcul de la réduction en pourcentage
                 $reduction = (($prixInitial - $prixPromo) / $prixInitial) * 100;
             ?>
-            <p>Nom de l'article : <?php echo $produit; ?></p>
-            <p>Le prix initial : <?php echo $prixInitial . " " . $devise; ?></p>
-            <p>Le prix en promotion : <?php echo $prixPromo . " " . $devise; ?></p>
-            <p>Réduction : -<?php echo round($reduction, 2); ?> %</p>
-            <br>
+            
+            <?php require 'composants/offre-card.php'; ?>
         <?php endforeach; ?>  
+        </div>
+        
     </div>
 <?php endforeach; ?>
 <?php require 'portion/footer.php' ?>
